ac65ef8333b31f9735ef8f530afe772d6ef242ed
Jordan <jordan.old@gmail.com>
2021-08-20 09:46:42 +1000

    Added addition function to file

--- CalculatorFunctions.js
+++ CalculatorFunctions.js
@@ -1,1 +1,11 @@
-
+function calcAddition()
+{
+    let num1 = parseFloat(prompt('enter 1st number : '))      
+    let num2 = parseFloat(prompt('enter 2nd number : '))
+    let op = prompt('enter operator : ')
+    let ans = 0;
+    if (op == '+')
+    {
+        return ans=num1+num2
+    }
+}
