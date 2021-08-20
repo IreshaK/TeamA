b2ce469353bf4cc4782a2ff67422de85b9c4abb6
Jordan <jordan.old@gmail.com>
2021-08-20 11:40:15 +1000

    Add addition function to code file

--- CalculatorFunctions.js
+++ CalculatorFunctions.js
@@ -1,12 +1,3 @@
-const prompt = require('prompt-sync')();
-
-function calculator()
-{
-    let num1 = parseFloat(prompt('enter 1st number : '))      
-    let num2 = parseFloat(prompt('enter 2nd number : '))
-    let operator = prompt('enter operator : ')
-    let answer = 0;
-    
     addition()    
     {
     if (operator == '+')
@@ -14,45 +5,3 @@ function calculator()
         return answer = num1 + num2;
     }
     }
-
-    subtraction()
-    {
-    if (operator == '+')
-    {
-        return answer = num1 + num2;
-    }
-    }
-
-    multiplication()
-    {
-    if (operator == '*')
-    {
-        return answer = num1 * num2;
-    }
-    }
-
-function calcDivision()
-{
-    let num1 = parseFloat (prompt ('enter 1st number : '))      
-    let num2 = parseFloat (prompt ('enter 2nd number : '))
-    let operator = prompt ('enter operator : ')
-    let answer = 0;
-    if (operator == '/')
-    {
-        return answer = num1 / num2;
-    }
-}
-
-function calcPower()
-{
-    let num1 = parseFloat (prompt ('enter 1st number : '))      
-    let num2 = parseFloat (prompt ('enter 2nd number : '))
-    let operator = prompt ('enter operator : ')
-    let answer = 0;
-    if (operator == '**')
-    {
-        return answer = num1 ** num2;
-    }
-}
-
-}
